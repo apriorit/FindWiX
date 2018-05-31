354c00f063597e7eddd0b8cf59049b17b9b8fbc5
Yulia Kostenko <sqzherimpsky@gmail.com>
2018-05-31 14:03:13 +0300

    Initial commit.

---
+++ README.md
@@ -0,0 +1,85 @@
+# FindWIX [![Build status](https://ci.appveyor.com/api/projects/status/4iagwdkceft3fb0o?svg=true)](https://ci.appveyor.com/project/sqzhr/findwix)
+
+Create a  new WIX cmake module
+* [Introduction](#introduction)
+* [Usage](#usage)
+* [Samples](#samples) 
+* [License](#license) 
+* [Version History](#version-history)
+
+# Introduction
+FindWIX makes it possible to build the WIX project for creating installers for Windows Installer
+
+Requirements:
+- CMake 3.0 and higher
+- WiX toolset
+
+# Usage
+Add FindWIX to the module search path and call find_package
+```cmake
+list(APPEND CMAKE_MODULE_PATH "${CMAKE_CURRENT_LIST_DIR}/cmake")
+find_package(WIX REQUIRED)
+```
+FindWIX will search for the installed WiX Toolset (WiX) and expose commands for creating installer file 
+Also the following variables will be defined:
+* ```WIX_FOUND``` -- if false, do not try to use WiX
+* ```WIX_ROOT``` -- where WiX is installed
+
+[findWiX.cmake](cmake/findWix.cmake) generates vars.wxi file of non-duplicated items list of all available variables and their values in xml format.
+Fragment of generated file content:
+```XML
+<?xml version='1.0' encoding='UTF-8'?>
+
+<Include>
+	<?define ARGC='4' ?>
+	<?define ARGN='Main.wxs;DEPENDS;CppExecutable' ?>
+	<?define ARGV='WithExecutable;Main.wxs;DEPENDS;CppExecutable' ?>
+	<?define ARGV0='WithExecutable' ?>
+	<?define ARGV1='Main.wxs' ?>
+	<?define ARGV2='DEPENDS' ?>
+	<?define ARGV3='CppExecutable' ?>
+	<?define CMAKE_AR='' ?>
+	<?define CMAKE_AUTOMOC_COMPILER_PREDEFINES='ON' ?>
+	<?define CMAKE_AUTOMOC_MACRO_NAMES='Q_OBJECT;Q_GADGET;Q_NAMESPACE' ?>
+```
+Also paths to cmake dependencies are stored in generated depends.wxi file.
+Fragment of generated file content:
+```XML
+<?xml version='1.0' encoding='UTF-8'?>
+
+<Include>
+	<?define TARGET_FILE:CppExecutable='C:/my_proj/WithExecutable/Debug/CppExecutable.exe' ?>
+	<?define TARGET_PDB_FILE:CppExecutable='C:/my_proj/WithExecutable/Debug/CppExecutable.pdb' ?>
+</Include>
+```
+
+
+##### wix add project
+```cmake
+wix_add_project(<name>
+  source1 [source2 ...]
+  [OUTPUT_NAME <msi_file_name>]
+  [EXTENSIONS extension1 [extension2...]]
+  [DEPENDS executable1 [executable2..]])
+```
+  * ```OUTPUT_NAME``` -- allow to set the name of a rezult .msi file, it can be set to an empty string, then the name of rezult file will be set to the <name> designation.
+  * ```EXTENSIONS``` -- add one or more extensions
+  * ```DEPENDS``` -- add files that should be built first
+  
+  Example:
+  ```cmake
+  wix_add_project(my_project 
+    main.wxs one_more.wxs 
+    OUTPUT_NAME "NameSample" 
+    EXTENSIONS WixUIExtension WixUtilExtension
+    DEPENDS CppExecutable)
+  ```  
+
+# Samples 
+Take a look at the [samples](samples/) folder to see how FindWiX is built.
+
+# License
+FindWIX is licensed under the OSI-approved 3-clause BSD license. You can freely use it in your commercial or opensource software.
+
+# Version History
+Also an example
---
+++ cmake/FindWix.cmake
@@ -0,0 +1,90 @@
+# Redistribution and use is allowed under the OSI-approved 3-clause BSD license.
+# Copyright (c) 2018 Apriorit Inc. All rights reserved.
+
+if(CMAKE_SIZEOF_VOID_P EQUAL 8)
+    set(WIX_ARCH "x64")
+else()
+    set(WIX_ARCH "x86")
+endif()
+
+file(GLOB WIX_FILES
+    "C:/Program Files*/WiX Toolset v*/bin"
+)
+
+if(WIX_FILES)
+    list(GET WIX_FILES -1 WIX_LATEST_FILE)
+endif()
+
+unset(${WIX_FILES})
+
+include(FindPackageHandleStandardArgs)
+find_package_handle_standard_args(WIX REQUIRED_VARS WIX_LATEST_FILE)
+
+if(NOT WIX_LATEST_FILE)
+    return()
+endif()
+
+get_filename_component(WIX_ROOT ${WIX_LATEST_FILE} DIRECTORY)
+message(STATUS "WIX_ROOT: " ${WIX_ROOT})
+
+unset(${WIX_LATEST_FILE})
+
+function(wix_add_project _target)
+    cmake_parse_arguments(WIX "" "OUTPUT_NAME" "EXTENSIONS;DEPENDS" ${ARGN})
+
+    if("${WIX_OUTPUT_NAME}" STREQUAL "")
+        set(WIX_OUTPUT_NAME "${_target}.msi")
+    endif()
+
+    foreach(currentFILE ${WIX_UNPARSED_ARGUMENTS})
+        GET_FILENAME_COMPONENT(SOURCE_FILE_NAME ${currentFILE} NAME_WE)
+        list(APPEND WIXOBJ_LIST "${SOURCE_FILE_NAME}.wixobj")
+        list(APPEND WIX_SOURCES_LIST ${CMAKE_CURRENT_LIST_DIR}/${currentFILE})
+    endforeach()
+
+    foreach(current_WIX_EXTENSION ${WIX_EXTENSIONS})
+        list(APPEND EXTENSION_LIST "-ext")
+        list(APPEND EXTENSION_LIST ${current_WIX_EXTENSION})
+    endforeach()
+
+    # Call wix compiler
+    add_custom_command(
+        OUTPUT ${CMAKE_CURRENT_BINARY_DIR}/${WIXOBJ_LIST}
+        COMMAND "${WIX_ROOT}/bin/candle.exe" -arch ${WIX_ARCH} ${WIX_COMPILE_FLAGS} -o "${CMAKE_CURRENT_BINARY_DIR}/" ${WIX_SOURCES_LIST} -I"${CMAKE_CURRENT_BINARY_DIR}" -I"${CMAKE_CURRENT_BINARY_DIR}/wxi/$<CONFIG>" -I"${CMAKE_CURRENT_BINARY_DIR}/wxi"
+        DEPENDS ${WIX_SOURCES_LIST}
+        COMMENT "Creating .wixobj file"
+        )
+
+    # Link MSI file
+    add_custom_command(
+        OUTPUT ${CMAKE_RUNTIME_OUTPUT_DIRECTORY}/${WIX_OUTPUT_NAME}
+        COMMAND "${WIX_ROOT}/bin/light.exe" ${WIX_LINK_FLAGS} -o ${WIX_OUTPUT_NAME} ${WIXOBJ_LIST} ${EXTENSION_LIST}
+        DEPENDS ${WIXOBJ_LIST} ${WIX_DEPENDS}
+        COMMENT "Creating ${WIX_OUTPUT_NAME} file"
+        )
+
+    add_custom_target(${_target} ALL
+        DEPENDS ${WIX_OUTPUT_NAME}
+        SOURCES ${WIX_UNPARSED_ARGUMENTS}
+        )
+
+    string(CONCAT DEPENDS_FILE "<?xml version='1.0' encoding='UTF-8'?>\n\n<Include>\n")
+    foreach(current_depends ${WIX_DEPENDS})
+        string(CONCAT DEPENDS_FILE  ${DEPENDS_FILE} "\t<?define TARGET_FILE:${current_depends}='$<TARGET_FILE:${current_depends}>' ?>\n")
+        string(CONCAT DEPENDS_FILE  ${DEPENDS_FILE} "\t<?define TARGET_PDB_FILE:${current_depends}='$<TARGET_PDB_FILE:${current_depends}>' ?>\n")
+    endforeach()
+    string(CONCAT DEPENDS_FILE ${DEPENDS_FILE} "</Include>")
+    file(GENERATE OUTPUT "${CMAKE_CURRENT_BINARY_DIR}/wxi/$<CONFIG>/depends.wxi" CONTENT "${DEPENDS_FILE}")
+    unset(DEPENDS_FILE)
+
+    get_cmake_property(WIX_variableNames VARIABLES)
+    set(VARS_PATH "${CMAKE_CURRENT_BINARY_DIR}/wxi/vars.wxi")
+    file(WRITE "${VARS_PATH}" "<?xml version='1.0' encoding='UTF-8'?>\n\n<Include>\n")
+    list(REMOVE_DUPLICATES WIX_variableNames)
+    foreach(WIX_variableName ${WIX_variableNames})
+        string(REPLACE "$" "$$" OUT "${WIX_variableName}='${${WIX_variableName}}'")
+        file(APPEND "${VARS_PATH}" "\t<?define ${OUT} ?>\n")
+    endforeach()
+    file(APPEND "${VARS_PATH}" "</Include>")
+
+endfunction()
